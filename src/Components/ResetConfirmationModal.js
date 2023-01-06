cf136ac48e415721fc30a7a491d6349a45907a07
Олег Шевченко <103051547+FatRedSquirrel@users.noreply.github.com>
2023-01-06 09:20:20 +0300

    изменил формулировку сообщения при подтверждении сброса

--- src/Components/ResetConfirmationModal.js
+++ src/Components/ResetConfirmationModal.js
@@ -2,7 +2,7 @@ export default function ResetConfirmationModal(props) {
     return (
         <div className='modal'>
             <div className='reset-confirmation'>
-                <h2>Вы уверены, что хотите сделать сброс? Все сохраненные данные будут очищены</h2>
+                <h2>Вы уверены, что хотите сделать сброс? Все сохраненные данные будут удалены</h2>
                 <div className="buttons">
                     <button className="reset-button" onClick={props.resetButtonClickHandler}>Сбросить</button>
                     <button className="cancel-button" onClick={props.cancelButtonClickHandler}>Отмена</button>
