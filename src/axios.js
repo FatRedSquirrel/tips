552aa5626ca9b0ebf7301f89c7741c308f4801b4
Олег Шевченко <103051547+FatRedSquirrel@users.noreply.github.com>
2023-02-19 20:39:43 +0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: 'https://tips-back.vercel.app/'
-    baseURL: 'http://localhost:666'
+    baseURL: 'https://tips-back.vercel.app/'
+    // baseURL: 'http://localhost:666'
 });
 
 export default instance;
